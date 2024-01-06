a2d1b8a2152500fc73e9509a072755345ea76a17
tomilin229 <tomilin229@gmail.com>
2024-01-07 01:13:09 +0300

    bug fix

---
+++ src/components/PhotoCard.tsx
@@ -0,0 +1,14 @@
+import type { IPhoto } from "../types/photos.ts";
+
+export default function PhotoCard(props: IPhoto) {
+  const { url, title, id } = props
+
+  return (
+    <a href={'/photo/' + id}>
+      <div className="mt-2 flex justify-between w-80 rounded-md bg-blue-200 p-2 gap-2">
+        <img width={120} height={120} src={url} alt={'post'}/>
+        <p className="font-semibold text-gray-900">{title}</p>
+      </div>
+    </a>
+  )
+}
