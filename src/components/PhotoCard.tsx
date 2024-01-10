d28c2b73dcec9dcc4bf512f56678a9f5a1f05731
tomilin229 <tomilin229@gmail.com>
2024-01-11 00:50:51 +0300

    bug fix

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -4,7 +4,7 @@ export default function PhotoCard(props: IPhoto) {
   const { url, title, id } = props
 
   return (
-    <a href={'/photo/' + id}>
+    <a href={'/photo/' + id} id={"photo" + id}>
       <div className="mt-2 flex justify-between w-80 rounded-md bg-blue-200 p-2 gap-2">
         <img width={120} height={120} src={url} alt={'post'}/>
         <p className="font-semibold text-gray-900">{title}</p>
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -5,7 +5,7 @@ export default function PostCard (props: IPost) {
   const { body, title, id } = props
 
   return (
-    <a href={'/post/' + id}>
+    <a href={'/post/' + id} id={"post" + id}>
       <div className="mt-4 flex w-100 rounded-md gap-10 bg-blue-200 p-2 border-r-4">
         <div>
           <span className="ml-auto font-bold text-gray-900">{title}</span>
